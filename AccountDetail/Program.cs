ffdbdbe9bcf0e6ecbbd1e67ac1ad58b1d563a51a
Munazzah Mustajab <mmustajab@hitachisolutions.com>
2018-06-13 14:07:35 +0530

    push

--- AccountDetail/Program.cs
+++ AccountDetail/Program.cs
@@ -13,14 +13,14 @@ namespace AccountDetail
 {
     class Program
     {
-        private static CrmServiceClient _client;
-
         public static void Main(string[] args)
         {
+
             try
             {
-                using (_client = new CrmServiceClient(ConfigurationManager.ConnectionStrings["CRMConnectionString"].ConnectionString))
+                using (var client = new CrmServiceClient(ConfigurationManager.ConnectionStrings["CRMConnectionString"].ConnectionString))
                 {
+                    var applicationSetting = ConfigurationManager.GetSection("appSettings") as NameValueConfigurationCollection;
 
                     Console.WriteLine("Login successfull...");
                     Console.Read();
@@ -28,84 +28,175 @@ namespace AccountDetail
                     Console.Read();
                     String entityName = Console.ReadLine();
                     Console.WriteLine("Entity you entered is " + entityName);
-                      if (entityName == "Account")
+
+
+                    if (entityName.Equals("Account")) //Account information
+                    {
+                        String fetchXml = @"<fetch mapping='logical' distinct='true'>
+                                                         <entity name='account'> 
+                                                         {0}
+                                                            </entity> 
+                                                       </fetch>";
+
+
+                        String attributeFormat = @"<attribute name = {0}>";
+
+                        String configAttribute = ConfigurationManager.AppSettings["acountKey"];
+
+                        String[] configAttSplit = configAttribute.Split(',');
+
+                        for (var i = 0; i <= configAttSplit.Length; i++)
                         {
 
-                            string fetchXMLAccount = @"
-                       <fetch mapping='logical'>
-                         <entity name='account'> 
-                            <attribute name='accountid'/> 
-                            <attribute name='name'/> 
+
+                            String attributefrmt = String.Format(configAttribute, attributeFormat[i]);
+
+                           String fetchString = String.Format(fetchXml, configAttSplit[i]);
                             
-                         </entity> 
-                       </fetch> ";
 
-                            IOrganizationService crmService = _client.OrganizationServiceProxy;
-                            EntityCollection result = crmService.RetrieveMultiple(new FetchExpression(fetchXMLAccount));
+                            IOrganizationService crmService = client.OrganizationServiceProxy;
+                            EntityCollection result = crmService.RetrieveMultiple(new FetchExpression(fetchString));
 
-                            if (result != null)
+
+                            if (result != null) //Check whether the value of result 
                             {
-                                foreach (var c in result.Entities)
+                                foreach (var entity in result.Entities)
                                 {
-                                    System.Console.WriteLine(c.Attributes["name"]);
+
+
+                                    if (entity.Attributes.Contains(configAttSplit[i]))
+                                    {
+                                        if (!string.IsNullOrEmpty(entity.GetAttributeValue<string>(configAttSplit[i])) && !string.IsNullOrEmpty(entity.GetAttributeValue<string>(configAttSplit[i+1])))
+                                        {
+
+                                            System.Console.Write(entity.Attributes[configAttSplit[i]]);
+                                            System.Console.WriteLine(entity.Attributes[configAttSplit[i+1]]);
+
+                                        }
+
+                                    }
+                                    else
+                                    {
+                                        return;
+                                    }
+
                                 }
                             }
                             else
                             {
-                                throw new System.NullReferenceException();
-                                
-
+                                Console.WriteLine("No records found for the account entity.");
                             }
-                            Console.Read();
-
                         }
 
+                    }
+                    else // contact information
+                    {
+                        String fetchXml = @"<fetch mapping='logical' distinct='true'>
+                                                         <entity name='contact'> 
+                                                         {0}
+                                                            </entity> 
+                                                       </fetch>";
 
-                        else
+
+                        String attributeFormat = @"<attribute name = {0}>";
+
+                        String configAttribute = ConfigurationManager.AppSettings["contactKey"];
+
+                        String[] configAttSplit = configAttribute.Split(',');
+
+                        for (var i = 0; i < configAttSplit.Length; i++)
                         {
 
-                            Console.WriteLine("Contact information");
 
-                            string fetchXmlContact = @"
-                      <fetch mapping='logical'>
-                        <entity name='contact'> 
-                           <attribute name='fullname'/> 
-                           <attribute name='contactid'/> 
+                            String attributefrmt = String.Format(configAttribute, attributeFormat[i]);
 
-                        </entity> 
-                      </fetch> ";
+                            String fetchString = String.Format(fetchXml, configAttSplit[i]);
 
-                            IOrganizationService crmService = _client.OrganizationServiceProxy;
-                            EntityCollection result = crmService.RetrieveMultiple(new FetchExpression(fetchXmlContact));
+                            IOrganizationService crmService = client.OrganizationServiceProxy;
+                            EntityCollection result = crmService.RetrieveMultiple(new FetchExpression(fetchString));
 
-                            if (result != null)
+                            if (result != null)//Check whether the value of result 
                             {
-                                foreach (var c in result.Entities)
+                                foreach (var entity in result.Entities)
                                 {
-                                    System.Console.WriteLine(c.Attributes["fullname"]);
+                                    if (entity.Attributes.Contains("name"))
+                                    {
+                                        System.Console.WriteLine(entity.Attributes["name"]);
+                                    }
+
                                 }
                             }
                             else
                             {
-
-                                throw new System.NullReferenceException();
+                                Console.WriteLine("No records found for the account entity.");
                             }
                         }
                     }
-                    
+                }
 
-                    Console.Read();
-                
-                 
-                
+                #region commented
+                /*if (entityName.Equals("Account"))
+                {
+                    string fetchXMLAccount = @"<fetch mapping='logical' distinct='true'>
+                                                     <entity name='account'> 
+                                                        <attribute name='accountid'/> 
+                                                        <attribute name='name'/> 
+                                                     </entity> 
+                                                   </fetch>";
+
+                    IOrganizationService crmService = client.OrganizationServiceProxy;
+                    EntityCollection result = crmService.RetrieveMultiple(new FetchExpression(fetchXMLAccount));
+                    if (result != null)
+                    {
+                        foreach (var entity in result.Entities)
+                        {
+                            if (entity.Attributes.Contains("name"))
+                            {
+                                System.Console.WriteLine(entity.Attributes["name"]);
+                            }
+
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("No records found for the account entity.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Contact information");
+                    string fetchXmlContact = @"<fetch mapping='logical'>
+                                                    <entity name='contact' distinct='true'> 
+                                                       <attribute name='fullname'/> 
+                                                       <attribute name='contactid'/> 
+                                                    </entity> 
+                                                 </fetch> ";
+
+                    IOrganizationService crmService = client.OrganizationServiceProxy;
+                    EntityCollection result = crmService.RetrieveMultiple(new FetchExpression(fetchXmlContact));
+                    if (result != null)
+                    {
+                        foreach (var entity in result.Entities)
+                        {
+                            if (entity.Attributes.Contains("fullname"))
+                            {
+                                System.Console.WriteLine(entity.Attributes["fullname"]);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("No records found for the contact entity.");
+                    }
+                }*/
+                #endregion
             }
 
-           
-            catch (FaultException<OrganizationServiceFault> ex)
+            catch
             {
-
                 throw;
             }
+            Console.Read();
         }
     }
 }
